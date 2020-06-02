ca9a48ab39560e0abd24af49befb61f2cb0f2e6b
Matheus I Lopes <matheuslopes1@protonmail.com>
2020-06-02 19:04:14 -0300

    navegando pro detalhe

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+import "./styles.css";
 
 const Product = () => {
-  return <h1>Product</h1>;
+  const [product, setProduct] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const getProduct = async () => {
+      try {
+        const response = await api().get(`/products/${id}`);
+
+        if (isMounted) {
+          setProduct(response.data);
+        }
+      } catch (err) {
+        console.warn({ message: err.message, detail: err });
+      }
+    };
+
+    getProduct();
+
+    return () => {
+      isMounted = false;
+    };
+  });
+
+  return (
+    <div className="product-info">
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+
+      <p>
+        URL: <a href={product.url}>{product.url}</a>
+      </p>
+    </div>
+  );
 };
 
 export default Product;
